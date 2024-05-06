01256bede329d045652edfe2c8e64ca05dc2b6d6
Ronnie <66483497+leverage-point@users.noreply.github.com>
2024-05-06 08:47:39 +0800

    update images (#42)
    1. draw svg figure in ch-ray-train-tune and change the figure file name in the text content.
    
    Note that for 'bayesian-optimization-explained.svg', the base image is drawn using code 'drawio\ch-ray-train-tune\Rplot.R', and the text on the image is added in draw.io; and for 'tune-algorithms.svg', the base image is drawn using code 'drawio\ch-ray-train-tune\Pythonplot.py' , and the text on the image is added in draw.io;
    
    Note that when exporting 'bayesian-optimization-explained.svg' from draw.io, do not select options '包含绘图副本', '嵌入图片', and '嵌入字体'.
    
    2. redraw 'img\ch-mpi\rma-window.svg', 'img\ch-mpi-large-model\gpu-direct.svg', 'img\ch-mpi-large-model\mpi-wo-gpu-direct.svg',  'img\ch-mpi-large-model\gpu-communication.svg'.

---
+++ drawio/ch-ray-train-tune/Pythonplot.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 24 18:49:12 2024
+
+@author: LY
+"""
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import numpy as np
+
+harvest = np.array([[2.8, 2.4, 2.6, 3.0, 3.2, 3.5, 3.2],
+                    [2.4, 2.3, 2.1, 2.5, 3.4, 6.0, 3.5],
+                    [3.1, 2.0, 0.1, 0.3, 3.2, 3.4, 2.8],
+                    [4.6, 2.5, 0.3, 1.2, 1.8, 1.6, 1.8],
+                    [6.0, 5.6, 2.6, 1.8, 0.1, 0.3, 0.8],
+                    [5.5, 4.5, 3.5, 1.5, 0.3, 0.0, 0.6],
+                    [4.5, 4.0, 3.0, 2.4, 0.8, 0.5, 0.2]])
+
+# https://plotly.com/python/builtin-colorscales/
+
+fig, ax = plt.subplots(figsize=(4, 4))
+im = ax.imshow(harvest,interpolation="gaussian",cmap='jet')
+ax.axis('off')
+rect = patches.Rectangle((-0.5, -0.5), harvest.shape[1], harvest.shape[0], linewidth=2, edgecolor='black', facecolor='none')
+ax.add_patch(rect)
+
+fig.savefig(r'C:\Users\LY\Desktop\figure.svg', format="svg")
