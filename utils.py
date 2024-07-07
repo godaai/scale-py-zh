dac68453ee2537be1efb3e02fd66670388a8ba7d
Dawnfz-Lenfeng <2912706234@qq.com>
2024-07-07 22:19:12 +0800

    dask case 2

--- utils.py
+++ utils.py
@@ -1,11 +1,45 @@
 import os
 import shutil
 import urllib
+from zipfile import ZipFile
+import functools
 
-def nyc_taxi(data_path: str = "../data/nyc-taxi"):
 
-    folder_path = os.path.join(os.getcwd(), "../data/nyc-taxi")
-    download_url = [
+def get_data(data_path, download_url: list[str]):
+    folder_path = os.path.join(os.getcwd(), "../data", data_path)
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+    for url in download_url:
+        file_name = url.split("/")[-1]
+        file_path = os.path.join(folder_path, file_name)
+        if not os.path.exists(file_path):
+            print(f"正在下载 {file_name}")
+            with urllib.request.urlopen(url) as response, open(
+                file_path, "wb"
+            ) as out_file:
+                shutil.copyfileobj(response, out_file)
+            if file_name.endswith(".zip"):
+                with ZipFile(file_path, "r") as zf:
+                    zf.extractall(folder_path)
+
+    return folder_path
+
+
+def data_path_decorator(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        data_path = func.__name__
+        download_url = func()
+        folder_path = get_data(data_path, download_url)
+        return folder_path
+
+    return wrapper
+
+
+@data_path_decorator
+def nyc_taxi():
+    return [
         "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-01.parquet",
         "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-02.parquet",
         "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-03.parquet",
@@ -13,30 +47,32 @@ def nyc_taxi(data_path: str = "../data/nyc-taxi"):
         "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-05.parquet",
         "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-06.parquet",
     ]
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
-    for url in download_url:
-        file_name = url.split("/")[-1]
-        parquet_file_path = os.path.join(folder_path, file_name)
-        if not os.path.exists(os.path.join(folder_path, file_name)):
-            print(f"正在下载 {file_name}")
-            with urllib.request.urlopen(url) as response, open(parquet_file_path, 'wb') as out_file:
-                shutil.copyfileobj(response, out_file)
-
-    return folder_path
 
 
-def nyc_flights(data_path: str = "../data/"):
-    folder_path = os.path.join(os.getcwd(), data_path)
-    download_url = "https://dp.godaai.org/nyc-flights.zip"
-    zip_file_path = os.path.join(folder_path, "nyc-flights.zip")
-    if not os.path.exists(os.path.join(folder_path, "nyc-flights")):
-        print(f"正在下载 nyc-flights.zip")
-        with urllib.request.urlopen(download_url) as response, open(zip_file_path, 'wb') as out_file:
-            shutil.copyfileobj(response, out_file)
-            zf = ZipFile(zip_file_path, 'r')
-            zf.extractall(folder_path)
-            zf.close()
+@data_path_decorator
+def nyc_flights():
+    return ["https://dp.godaai.org/nyc-flights.zip"]
 
-    folder_path = os.path.join(folder_path, "nyc-flights")
-    return folder_path
+
+@data_path_decorator
+def citi_bike():
+    return [
+        "https://s3.amazonaws.com/tripdata/JC-202301-citibike-tripdata.csv.zip",
+        "https://s3.amazonaws.com/tripdata/JC-202302-citibike-tripdata.csv.zip",
+        "https://s3.amazonaws.com/tripdata/JC-202303-citibike-tripdata.csv.zip",
+        "https://s3.amazonaws.com/tripdata/JC-202304-citibike-tripdata.csv.zip",
+    ]
+
+
+@data_path_decorator
+def more_citi_bike():
+    return [
+        "https://s3.amazonaws.com/tripdata/JC-202310-citibike-tripdata.csv.zip",
+        "https://s3.amazonaws.com/tripdata/JC-202311-citibike-tripdata.csv.zip",
+        "https://s3.amazonaws.com/tripdata/JC-202312-citibike-tripdata.csv.zip",
+    ]
+
+
+@data_path_decorator
+def adult():
+    return ["https://archive.ics.uci.edu/static/public/2/adult.zip"]
